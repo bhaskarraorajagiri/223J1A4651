7489a25d8252ab4b8e3e26de4dbc04bdbc66d997
bhaskarrajagiri <bhaskarraorajagiri@gmail.com>
2025-08-05 11:04:15 +0530

    logger add

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import shortUrlRoutes from './routes/shortUrl.route.js';
+import urlRoutes from './routes/url.routes.js';
+import { logger } from './middlewares/logger.js';
 
 dotenv.config();
 
 const app = express();
-
-connectDB();
-
-app.use(express.json());
-app.use('/', shortUrlRoutes);
-
-
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(logger); 
+
+// Routes
+app.use('/', urlRoutes);
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
+  connectDB();
 });
